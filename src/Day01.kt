748de121b13d72c60515f3dde69ce2413474948c
Ian Redman <i.redman@gymshark.com>
2021-12-01 09:30:45 +0000

    Day 1 ✅

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,16 +1,40 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var increments = 0
+
+        input.forEachIndexed { index, depth ->
+            if (index > 0) {
+                if (depth.toInt() > input[index - 1].toInt()) {
+                    increments = increments.plus(1)
+                }
+            }
+        }
+
+        return increments
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var increments = 0
+
+        input.forEachIndexed { index, _ ->
+            if (index.plus(3) < input.size) {
+                val firstWindow = input[index].toInt()
+                    .plus(input[index + 1].toInt())
+                    .plus(input[index + 2].toInt())
+
+                val secondWindow = input[index + 1].toInt()
+                    .plus(input[index + 2].toInt())
+                    .plus(input[index + 3].toInt())
+
+                if (secondWindow > firstWindow) {
+                    increments = increments.plus(1)
+                }
+            }
+        }
+
+        return increments
     }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
+    
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
